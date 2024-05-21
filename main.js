46473a5004f64558458f980800846f212ea777f0
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2024-05-21 16:59:26 -0500

    Add Button Components

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -19,6 +18,4 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+`;
